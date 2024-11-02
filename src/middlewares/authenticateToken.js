d742ea38de9792a56ab1d4d73ed69bbf8a75c9dd
Juan Camilo <juancamilopaco@ufps.edu.co>
2024-11-01 21:16:09 -0500

    .

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -2,8 +2,9 @@ import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "../config.js";
 
 const authenticateToken = (req, res, next) => {
+    console.log(req.cookies)
   const token = req.cookies.access_token; // Obtén el token de la cookie
-
+    
   if (!token) {
     console.log(token)
     return res.status(401).json({ message: "No autenticado" });
